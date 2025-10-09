ef19953d592276526fa088155692c4b7e4dccf2a
Martin Morgan <mtmorgan.xyz@gmail.com>
2025-10-09 04:55:58 -0400

    refactor code
    - more code inside 'sketch'
    - redraw plot data during each image expanstion to avoid 'streaks'
    - add label at beginning of load for better debugging

--- src/lib/MushroomPlot.svelte
+++ src/lib/MushroomPlot.svelte
@@ -2,10 +2,15 @@
 	import { resolve } from '$app/paths';
 	import P5, { type Sketch } from 'p5-svelte';
 
+	// Sketch configuration
+
 	let clientWidth: number;
 	let width: number;
 	let height: number;
 	let stroke = 12;
+	const backgroundColor = 220;
+
+	// Data
 
 	interface FileMap {
 		// EXIF data fields
@@ -28,18 +33,33 @@
 		Martin: '#FC8D62' // Salmon
 	};
 
+	// Image
+
 	const IMAGE_PREFIX: string = resolve('/images') + '/';
 	const url: string = IMAGE_PREFIX + 'mushrooms.json';
 	let selectedImagePath: string;
 	let selectedImage: any = null;
 
-	// image expansion
+	// Image expansion
+
 	let startX: number; // Initial x-coordinate
 	let startY: number; // Initial y-coordinate
 	let initialSize = 20; // Initial size (width and height)
 	let initialFrameCount = 0;
 	let expansionSpeed = 0.01; // How fast the image expands
 	let updateComplete = false;
+
+	// Fetch and transform data helpers
+
+	const range = (x: number[]): [number, number] => {
+		return [Math.min(...x), Math.max(...x)];
+	};
+
+	const scale = (x: number, range: [number, number], to: number) => {
+		const pad = 0.05;
+		const s0 = (x - range[0]) / (range[1] - range[0]);
+		return to * (pad + (1 - 2 * pad) * s0);
+	};
 
 	const fetchData = async (url: string) => {
 		const response = await fetch(url);
@@ -63,65 +83,9 @@
 		});
 	};
 
-	const range = (x: number[]): [number, number] => {
-		return [Math.min(...x), Math.max(...x)];
-	};
+	// sketch
 
-	const scale = (x: number, range: [number, number], to: number) => {
-		const pad = 0.05;
-		const s0 = (x - range[0]) / (range[1] - range[0]);
-		return to * (pad + (1 - 2 * pad) * s0);
-	};
-
-	const selectImage = async (p5, file: FileMap) => {
-		p5.noLoop();
-		selectedImagePath = IMAGE_PREFIX + file.FileName;
-		startX = p5.mouseX;
-		startY = p5.mouseY;
-		p5.loadImage(selectedImagePath, (img) => {
-			// Use callback rather than await to ensure that image is fully loaded?
-			// Start expansion from mouse position
-			initialFrameCount = p5.frameCount;
-			selectedImage = img;
-			p5.loop();
-		});
-	};
-
-	const updateImage = (p5) => {
-		// Calculate current size based on expansion progress
-		const frameCount = p5.frameCount - initialFrameCount;
-		let currentSize =
-			initialSize + (width - initialSize) * frameCount * expansionSpeed;
-
-		// Calculate current position to expand from the center of the initial point
-		let currentX = startX;
-		let currentY = startY;
-
-		// If the image is expanding beyond its initial point, adjust its position
-		// to expand towards the center of the canvas
-		if (currentSize > initialSize) {
-			let expansionRatio = (currentSize - initialSize) / (width - initialSize);
-			currentX = p5.lerp(startX, width / 2, expansionRatio);
-			currentY = p5.lerp(startY, height / 2, expansionRatio);
-		}
-
-		// Draw the image with the calculated size and position
-		const currentHeight =
-			currentSize * (selectedImage.height / selectedImage.width);
-		p5.image(selectedImage, currentX, currentY, currentSize, currentHeight);
-
-		// Stop loop when at full width. FIXME: full width or height
-		if (currentSize >= width) updateComplete = true;
-	};
-
-	const removeImage = (p5) => {
-		selectedImage = null;
-		selectedImagePath = '';
-		updateComplete = false;
-		p5.loop();
-	};
-
-	const mushroomSketch: Sketch = async (p5) => {
+	const sketch: Sketch = async (p5) => {
 		p5.setup = () => {
 			width = Math.min(clientWidth, 800);
 			height = width;
@@ -129,33 +93,106 @@
 			p5.imageMode(p5.CENTER);
 		};
 
+		// Utility
+
+		const inform = (message: string) => {
+			// Display message at bottom left of sketch
+			p5.strokeWeight(0);
+			p5.fill('white').rect(9, height - 8, p5.textWidth(message) + 2, -13);
+			p5.fill('black').text(message, 10, height - 10);
+		};
+
+		// Data load and display
+
 		const data: FileMap[] = await fetchData(url);
 		scaleData(data, width, height);
 
 		const plotData = () => {
-			p5.background(220);
+			p5.background(backgroundColor);
 			p5.strokeWeight(stroke);
 			data.forEach((d: FileMap) => {
-				p5.stroke(d.color);
-				p5.point(d.x, d.y);
+				p5.stroke(d.color).point(d.x, d.y);
 			});
-			p5.noLoop();
+		};
+
+		// Image load and display
+
+		const labelImage = () => {
+			const id = selectedImagePath.substring(
+				selectedImagePath.length - 13,
+				selectedImagePath.length - 5
+			);
+			inform(id);
+		};
+
+		const selectImage = async (file: FileMap) => {
+			selectedImagePath = IMAGE_PREFIX + file.FileName;
+			labelImage();
+			p5.loadImage(
+				selectedImagePath,
+				(img) => {
+					// Use callback rather than await to ensure that image is fully loaded?
+					// Start expansion from mouse position
+					startX = p5.mouseX;
+					startY = p5.mouseY;
+					initialFrameCount = p5.frameCount;
+					selectedImage = img;
+					p5.loop();
+				},
+				(event: Event) => {
+					inform('Error loading image');
+				}
+			);
+		};
+
+		const updateImage = (p5) => {
+			// Redraw the data layer so images expanding toward center don't 'streak'
+			plotData();
+
+			// Calculate current size based on expansion progress
+			const frameCount = p5.frameCount - initialFrameCount;
+			let currentSize =
+				initialSize + (width - initialSize) * frameCount * expansionSpeed;
+
+			// Calculate current position to expand from the center of the initial point
+			let currentX = startX;
+			let currentY = startY;
+
+			// If the image is expanding beyond its initial point, adjust its position
+			// to expand towards the center of the canvas
+			if (currentSize > initialSize) {
+				let expansionRatio =
+					(currentSize - initialSize) / (width - initialSize);
+				currentX = p5.lerp(startX, width / 2, expansionRatio);
+				currentY = p5.lerp(startY, height / 2, expansionRatio);
+			}
+
+			// Draw the image with the calculated size and position
+			const currentHeight =
+				currentSize * (selectedImage.height / selectedImage.width);
+			p5.image(selectedImage, currentX, currentY, currentSize, currentHeight);
+
+			// Stop loop when at full width. FIXME: full width or height
+			if (currentSize >= width) updateComplete = true;
+		};
+
+		const removeImage = () => {
+			selectedImage = null;
+			selectedImagePath = '';
+			updateComplete = false;
+			plotData();
 		};
 
 		p5.draw = () => {
 			if (selectedImage) {
 				updateImage(p5);
+				labelImage();
 				if (updateComplete) {
 					p5.noLoop();
-					const id = selectedImagePath.substr(selectedImagePath.length - 13, 8);
-					p5.strokeWeight(0);
-					p5.fill('white');
-					p5.rect(9, height - 8, 58, -13);
-					p5.fill('black');
-					p5.text(id, 10, height - 10);
 				}
 			} else {
 				plotData();
+				p5.noLoop();
 			}
 		};
 
@@ -164,13 +201,16 @@
 				removeImage(p5);
 				return;
 			}
-			let filtered: FileMap[] = data.filter(
-				(d) => p5.dist(p5.mouseX, p5.mouseY, d.x, d.y) < stroke / 2
-			);
-			if (filtered.length) {
-				const i = Math.floor(p5.random(filtered.length));
-				selectImage(p5, filtered[i]);
-			}
+			let nearest: FileMap | null = null;
+			let distance = 20;
+			data.forEach((d) => {
+				let thisDistance = p5.dist(p5.mouseX, p5.mouseY, d.x, d.y)
+				if (thisDistance < distance) {
+					distance = thisDistance;
+					nearest = d;
+				}
+			});
+			if (nearest) selectImage(nearest);
 		};
 
 		p5.windowResized = () => {
@@ -183,5 +223,5 @@
 </script>
 
 <div class="sketch-container" bind:clientWidth>
-	<P5 sketch={mushroomSketch} />
+	<P5 {sketch} />
 </div>
