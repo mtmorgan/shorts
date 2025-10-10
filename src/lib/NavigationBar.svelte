b9fe765303052488d3aceab19bc3c6cefb837c93
Martin Morgan <mtmorgan.xyz@gmail.com>
2025-10-10 03:20:25 -0400

    improve NavigationBar
    - collapsed by default on small screen

--- src/lib/NavigationBar.svelte
+++ src/lib/NavigationBar.svelte
@@ -1,32 +1,40 @@
 <script lang="ts">
 	import {
 		Container,
+		Collapse,
 		Navbar,
 		NavbarBrand,
 		Nav,
 		NavItem,
-		NavLink
+		NavLink,
+		NavbarToggler
 	} from '@sveltestrap/sveltestrap';
+
+	// Track window width for responsive navbar. '576' is the 'sm' breakpoint.
+	// See https://getbootstrap.com/docs/5.3/layout/breakpoints/
+	let innerWidth: number;
+	let isOpen = false;
 </script>
 
-<Container fluid>
-	<header class="site-header">
-		<Navbar container="sm" theme="light">
-			<NavbarBrand href="https://mtmorgan.github.io" class="me-auto"
-				>Martin Morgan</NavbarBrand
-			>
-			<Nav>
-				<NavItem>
-					<NavLink href="https://mtmorgan.github.io/bioconductor"
-						>R / Bioconductor</NavLink
-					>
-				</NavItem>
-				<NavItem>
-					<NavLink href="https://mtmorgan.github.io/javascript"
-						>JavaScript</NavLink
-					>
-				</NavItem>
-			</Nav>
-		</Navbar>
-	</header>
-</Container>
+<svelte:window bind:innerWidth />
+
+<Navbar container="sm" expand="sm" theme="light">
+	<NavbarBrand href="https://mtmorgan.github.io" class="me-auto"
+		>Martin Morgan</NavbarBrand
+	>
+	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+	<Collapse isOpen={innerWidth > 576 || isOpen} navbar>
+		<Nav class="ms-auto" navbar>
+			<NavItem>
+				<NavLink href="https://mtmorgan.github.io/bioconductor"
+					>R / Bioconductor</NavLink
+				>
+			</NavItem>
+			<NavItem>
+				<NavLink href="https://mtmorgan.github.io/javascript"
+					>JavaScript</NavLink
+				>
+			</NavItem>
+		</Nav>
+	</Collapse>
+</Navbar>
